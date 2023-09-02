1348bca52674ab379f5f119bd9a7ba0ce7a66db0
Younesid12 <idyounes95@gmail.com>
2023-09-01 20:01:36 -0700

    prints all the arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into the program
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc, char *argv[]);
+{
+	for (int a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[a++]);
+	}
+	return (0);
+}
