3155ba6d5e7f0d57877437c521063b6311f648c5
Younesid12 <idyounes95@gmail.com>
2023-09-01 19:48:56 -0700

    prints the number of the arguments passed followed by a new line

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the number of values passed into it followed by a new line
+ * @argc: number of arguments
+ * @argv: pointer to a character
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d/n", argc);
+	return (0);
+}
