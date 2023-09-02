cc31e06e6fc14dd3b4f1614e7b0ed58c2720eb90
Younesid12 <idyounes95@gmail.com>
2023-09-02 09:58:28 -0700

    prints the number of arguments passed into it.

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,6 +3,7 @@
  * main - prints the number of values passed into it followed by a new line
  * @argc: number of arguments
  * @argv: pointer to a character
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
