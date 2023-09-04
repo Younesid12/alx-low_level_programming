bbfc56f2c3df06ce592e3aeba56ce13243c2d086
Younesid12 <idyounes95@gmail.com>
2023-09-03 21:03:45 -0700

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	{
 		for (b = 1; argc > b; b++)
 		{
-			if (!isdigit(*(argv[b])) || atoi(argv[b]) < 0)
+			if (atoi(argv[b]) >= '0' && atoi(argv[b]) <= '9')
 			{
 				printf("Error\n");
 				return (1);
