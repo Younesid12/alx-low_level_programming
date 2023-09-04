754c9fa066d967943814a79f8ef0b28c167a125a
Younesid12 <idyounes95@gmail.com>
2023-09-04 08:25:33 -0700

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,25 +11,18 @@ int main(int argc, char *argv[])
 	int sum = 0;
 	int b;
 
-	if (argc == 1)
+	for (b = 1; argc > b; b++)
 	{
-		printf("0\n");
-	}
-	else
-	{
-		for (b = 1; argc > b; b++)
+		if (*(argv[b]) < '0' || *(argv[b]) > '9')
 		{
-			if (*(argv[b]) < '0' || *(argv[b]) > '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
-			else
-			{
-				sum += atoi(argv[b]);
-			}
+			printf("Error\n");
+			return (1);
 		}
-		printf("%d\n", sum);
+		else
+		{
+			sum += atoi(argv[b]);
+		}
 	}
+	printf("%d\n", sum);
 	return (0);
 }
