5aff63f2248665dd025c49fac4bd71a0f086c6dd
Younesid12 <idyounes95@gmail.com>
2023-09-06 16:35:08 -0700

    concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -29,9 +29,13 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	int i;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return ("");
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 
 	len1 = strl(s1);
