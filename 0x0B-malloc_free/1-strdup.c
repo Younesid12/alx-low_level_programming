4341faf2285ce9210eefdb39bbfe371499dd6fc8
Younesid12 <idyounes95@gmail.com>
2023-09-05 18:55:40 -0700

    returns a pointer to a newly allocated space containing a copied string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * strl - returns a length of a string
+ * @s: pointer to a string to be counted character by character
+ * Return: length
+ */
+int strl(char *s)
+{
+	int length;
+
+	for (length = 1; *s != '\0'; s++)
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
+ * _strdup - returns a pointer to allocated space containing a copy of a string
+ * @str: a pointer to a stringto be copied
+ * Return: pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i = 0;
+	int len;
+
+	len = strl(str);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	ptr = malloc(sizeof(char) * len + 1);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	while (len > 1)
+	{
+		ptr[i++] = str[i];
+	}
+	return (ptr);
+}
