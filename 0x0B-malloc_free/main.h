31584f8111b0bf0eb73a62a0bbad8f8ccda660a6
Younesid12 <idyounes95@gmail.com>
2023-09-08 15:34:30 -0700

    concatenates two string

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,4 +6,6 @@ int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 int strl(char *s);
+int _strlen(char *s);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 #endif
