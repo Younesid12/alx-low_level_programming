907f245df90b41a9a391dbb6932e2c3985336008
Younesid12 <idyounes95@gmail.com>
2023-09-12 17:35:20 -0700

    define a struct for dogs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Description: struct for dogs
+ */
 struct dog {
 	char *name;
 	float age;
 	char *owner;
-	/* struct dog can be used for handling info abot dogs */
 };
 #endif /* DOG_H */
