b3d973bf598548fa40887b69a0f35f7a526b7049
Younesid12 <idyounes95@gmail.com>
2023-09-12 19:42:30 -0700

    prints a struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,13 +1,13 @@
 #include "dog.h"
 #include <stdio.h>
+
 /**
- * print_dog - prints a struct dog
- * @d: pointer to the struct dog variable
- * Return: void
+ * print_dog - prints a dog
+ * @d: the dog to print
+ *
+ * Return: void.
  */
 void print_dog(struct dog *d)
-{
-	void print_dog(struct dog *d)
 {
 	if (d)
 	{
