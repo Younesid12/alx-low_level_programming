72098b3f5abdf7bcf4f5de341fb7ef0bc1812ab5
Younesid12 <idyounes95@gmail.com>
2023-09-12 19:02:23 -0700

    initializes a variable of type

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,7 +5,8 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- *Return: nothing
+ *
+ *Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
