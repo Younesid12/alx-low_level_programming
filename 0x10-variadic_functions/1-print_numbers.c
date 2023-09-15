68897a04bcf503d8b3d05056f622339a05c9f78d
Younesid12 <idyounes95@gmail.com>
2023-09-15 15:46:41 -0700

    prints numbers followed by a new line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		result = va_arg(args, const unsigned int);
 		printf("%d", result);
-		if (i != n - 1 && *separator != NULL)
+		if (i != n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
 		}
