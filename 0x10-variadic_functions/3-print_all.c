5c12cfb2d1bb55aa4b2a8a1a5193eba6d679dd22
Younesid12 <idyounes95@gmail.com>
2023-09-16 04:59:30 -0700

    prints anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_all - prints everything
+ * @format: list of types of arguments passed to the function
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	int i = 0;
+	char *str;
+
+	va_start(list, format);
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", (char)va_arg(list, int));
+				break;
+			case 'f':
+				printf("%f", (float)va_arg(list, double));
+				break;
+			case 'i':
+				printf("%d", va_arg(list, int));
+				break;
+			case 's':
+				str = va_arg(list, char*);
+				if (str == NULL)
+				{
+					printf("(nil)");
+				}
+				printf("%s", str);
+				break;
+			default:
+				break;
+		}
+		if (format[i + 1] != '\0' &&
+				(format[i] == 'c' || format[i] == 'f'
+				 || format[i] == 'i' || format[i] == 's'))
+		{
+		printf(", ");
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(list);
+}
+
+
+
+
