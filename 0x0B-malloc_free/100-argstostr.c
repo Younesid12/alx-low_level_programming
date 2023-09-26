5896072aad7a41293774b6ab801de7106f50f092
Younesid12 <idyounes95@gmail.com>
2023-09-26 00:15:13 -0700

    concatenates the arguments passed

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *argstostr - concatenates all arguments
+ * @ac: number of arguments
+ * @av: pointer to pointer to strings that are arguments passed
+ * Return: pointer to a string
+ */
+char *argstostr(int ac, char **av)
+{
+	int i;
+	int j;
+	int size;
+	int a;
+	char *result;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			size++;
+		}
+		size++;
+	}
+
+	result = malloc(sizeof(char) * size);
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			result[a] = av[i][j];
+			a++;
+		}
+		result[a++] = '\n';
+	}
+	result[a++] = '\0';
+
+	return (result);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,6 +6,4 @@ int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 int strl(char *s);
-int _strlen(char *s);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
 #endif
