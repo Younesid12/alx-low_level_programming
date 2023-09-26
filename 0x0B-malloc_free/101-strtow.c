6e8077f62ee1ce279335eb0806b0c4c625fa6680
Younesid12 <idyounes95@gmail.com>
2023-09-26 03:35:07 -0700

    first commit

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * **strtow - splits a string into words
+ * @str: a pointer to a string to be split into words
+ * Retrun: pointer to an array of strings
+ */
+char **strtow(char *str)
+{
