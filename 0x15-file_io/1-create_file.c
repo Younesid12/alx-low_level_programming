f23d6f71c6d695d5aeeaba5125ad3c5e5b502ba9
Younesid12 <idyounes95@gmail.com>
2023-10-20 08:51:52 -0700

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,7 +33,10 @@ int create_file(const char *filename, char *text_content)
 	{
 		fd = open(filename, O_WRONLY | O_TRUNC);
 	}
-	fd = open(filename, O_CREAT | O_WRONLY, 700);
+	else
+	{
+		fd = open(filename, O_CREAT | O_WRONLY, 600);
+	}
 	if (fd == -1)
 	{
 		return (-1);
@@ -45,9 +48,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	else if (*text_content == ' ')
-	{
 		free(text_content);
 		close(fd);
-	}
 	return (1);
 }
