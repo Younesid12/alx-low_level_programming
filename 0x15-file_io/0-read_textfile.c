78c2e8ea8a32be95bd0edced8bb44796e2f11b47
Younesid12 <idyounes95@gmail.com>
2023-10-30 19:00:15 -0700

    reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include <main.h>
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: pointer to the filename to be read
+ * @letters: number of letters to be read
+ * Return: number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytes_num;
+	ssize_t bytes_wrote;
+	char buffer[1024];
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+	else
+	{
+		bytes_num = read(fd, buffer, letters);
+		if (bytes_num == -1)
+		{
+			close(fd);
+			return (0);
+		}
+		else
+		{
+			bytes_wrote = write(STDOUT_FILENO, buffer, letters);
+			if (bytes_wrote == -1 || bytes_wrote != letters)
+			{
+				close(fd);
+				return (0);
+			}
+			else
+			{
+				close(fd);
+				return (bytes_wrote);
+			}
+		}
+	}
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_h
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stddef.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif /* MAIN_h */
