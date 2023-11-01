ef6ede7af777ab94f7ae7d7899be67cc1cfcfae2
Younesid12 <idyounes95@gmail.com>
2023-11-01 14:56:07 -0700

    creates a file and writes content to it

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: pointer to a filename
+ * @text_content: pointer to the text content
+ * Return: 1 on success, otherwise -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t letters;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+	letters = strlen(text_content);
+
+	if (buffer == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_TRUNC);
+	if (fd != -1)
+	{
+		if (filename != NULL)
+			bytes_written = write(fd, text_content, letters);
+			if (bytes_written == -1)
+				close(fd);
+				return (-1);
+		if (filename == NULL || bytes_written != -1)
+			close(fd);
+			return (1);
+	}
+	fd = open(filename, O_WRONLY | O_CREAT, 0600);
+	if (fd == -1)
+		return (-1);
+	if (filename != NULL)
+	{
+		bytes_written = write(fd, buffer, letters);
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	if (filename == NULL || bytes_written != 1)
+		close(fd);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,12 @@
 
 #include <stddef.h>
 #include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif /* MAIN_h */
