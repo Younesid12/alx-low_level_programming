9eebb40ae3ef135e8ce519ebc1f214e9db213f2c
Younesid12 <idyounes95@gmail.com>
2023-10-31 17:26:26 -0700

    reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,8 @@
-#ifndef MAIN_h
+#ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
 #include <stddef.h>
-#include <fcntl.h>
+#include <sys/types.h>
 ssize_t read_textfile(const char *filename, size_t letters);
+
 #endif /* MAIN_h */
