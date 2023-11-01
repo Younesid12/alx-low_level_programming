d78f8f9f208a6e91d124a365c3414921dca3e5db
Younesid12 <idyounes95@gmail.com>
2023-10-31 17:50:32 -0700

    reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -32,12 +32,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 	else
 	{
-		check = letters;
 		bytes_num = read(fd, buffer, letters);
 		if (bytes_num != -1)
 		{
 			bytes_wrote = write(STDOUT_FILENO, buffer, bytes_num);
-			if (bytes_wrote != check)
+			if (bytes_wrote != bytes_num)
 			{
 				free(buffer);
 				close(fd);
