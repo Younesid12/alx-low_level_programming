d40c8569db7bf26da8def9897161f9e13fa18d06
Younesid12 <idyounes95@gmail.com>
2023-11-01 19:11:29 -0700

    creates a file and writes content to it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,16 +31,11 @@ int create_file(const char *filename, char *text_content)
 				close(fd);
 				return (-1);
 			}
-			else
-			{
-				close(fd);
-				return (1);
-			}
 		}
 		else
 		{
 			close(fd);
-			return (-1);
+			return (1);
 		}
 	}
 }
