c573dcbb2b4d8617f5a837cfe6bc133573f5bb69
Younesid12 <idyounes95@gmail.com>
2023-11-02 16:33:04 -0700

    creates a file and writes something to it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,18 +10,20 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t letters;
 	ssize_t bytes_written;
+	int check;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
+	letters = strlen(text_content);
 	check = access(filename, F_OK);
 	if (check == 0)
 	{
 		fd = open(filename, O_WRONLY);
 		if (fd == -1)
 		{
-			return (-1)
+			return (-1);
 		}
 	}
 	else
@@ -29,10 +31,10 @@ int create_file(const char *filename, char *text_content)
 		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 		if (fd == -1)
 		{
-			return (-1)
+			return (-1);
 		}
 	}
-	if (filename != NULL)
+	if (text_content != NULL)
 	{
 		bytes_written = write(fd, text_content, letters);
 		if (bytes_written == -1)
