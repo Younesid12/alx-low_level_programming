b5c5747aa269f8abf70e0ec6f212c2ed55e89f89
Younesid12 <idyounes95@gmail.com>
2023-11-02 13:16:44 -0700

    creates a file and writes some content to it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -37,10 +37,13 @@ int create_file(const char *filename, char *text_content)
 				return (1);
 			}
 		}
-		else
+		bytes_written = write(fd, NULL, letters);
+		if (bytes_written == -1)
 		{
 			close(fd);
-			return (1);
+			return (-1);
 		}
+		close(fd);
+		return (1);
 	}
 }
