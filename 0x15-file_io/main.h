70e229042258a5198df1ca592052007b49e7af70
Younesid12 <idyounes95@gmail.com>
2023-11-04 14:36:53 -0700

    copies the content of a file to another file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,7 +12,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-char *create_buffer(char *filename);
 void _close(int fd);
 
 #endif /* MAIN_h */
