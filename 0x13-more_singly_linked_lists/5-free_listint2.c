fbe1e64f9e39089af78e6dbfa35a23053082ea33
Younesid12 <idyounes95@gmail.com>
2024-01-05 02:43:15 -0800

    frees a listint_t list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,14 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
-
-	while (*head != NULL)
+	
+	if(head)
 	{
-		ptr = (*head)->next;
-		free(*head);
-		*head = ptr;
+		while (*head != NULL)
+		{
+			ptr = (*head)->next;
+			free(*head);
+			*head = ptr;
+		}
 	}
 }
