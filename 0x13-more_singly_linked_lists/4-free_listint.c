8b50878ca0508c60d950816f27ad37b6649c5d2d
Younesid12 <idyounes95@gmail.com>
2024-01-05 01:47:10 -0800

    a function that frees a listinit list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,6 +2,11 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include "lists.h"
+/**
+ * free_listint - a function that frees a listint list
+ * @head: pointer to the first node of the list
+ * Return: void
+ */
 void free_listint(listint_t *head)
 {
 	listint_t *ptr;
