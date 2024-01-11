2b99899123c75617feb5415be21ba4eb5679a85b
Younesid12 <idyounes95@gmail.com>
2024-01-11 12:02:57 -0800

    inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp;
 	listint_t *new_node;
 	unsigned int i;
-	int count = 0;
+	unsigned int count = 0;
 
 	if (!head || !*head)
 		return (NULL);
