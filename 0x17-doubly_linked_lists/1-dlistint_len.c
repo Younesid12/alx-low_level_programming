1e88738a7ab8c986fc7026777dee733f4be55e2f
Younesid12 <idyounes95@gmail.com>
2024-01-12 03:05:00 -0800

    returns the number of elements in a linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * dlistint_len - returns the number if elements in a linked dlistint_t list
+ * @h: the head of the linked list
+ * Return: the number of elements in a linked list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint *ptr;
+	size_t count;
+
+	count = 0;
+	ptr = h;
+
+	while (ptr != NULL)
+	{
+		count++;
+		ptr = ptr->next;
+	}
+	return (count);
+}
