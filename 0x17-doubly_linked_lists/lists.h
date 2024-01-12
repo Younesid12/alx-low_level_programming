ec8ec89789951956cca0374b15da828aa63ce97f
Younesid12 <idyounes95@gmail.com>
2024-01-12 02:28:22 -0800

    prints all the elements of a dlistint_t list

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -4,6 +4,12 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdio.h>
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
 
