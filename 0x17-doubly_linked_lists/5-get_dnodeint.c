0bad2c4af46e90386fbd5712c0b33f8f20d4c9e1
Younesid12 <idyounes95@gmail.com>
2024-01-13 07:53:17 -0800

    returns the nth node of a dlistint_t linked list

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -5,7 +5,7 @@
  * @index: the index of the node starting from 0
  * Return: the nth node of a dlistint_t linked list, NULL if it doesn't exist
  */
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *ptr;
 	unsigned int count;
