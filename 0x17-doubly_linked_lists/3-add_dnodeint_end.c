4db01b11994d3511072b13660a5580fab23ec6df
Younesid12 <idyounes95@gmail.com>
2024-01-13 05:23:38 -0800

    adds a new node at the end if a dlistint_t list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 
 	ptr = *head;
-	nwe_node = malloc(sizeof(dlistint_t));
+	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 	{
 		return (NULL);
@@ -29,7 +29,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (new_node);
 	}
 	ptr = *head;
-	while(ptr->next != NULL)
+	while (ptr->next != NULL)
 	{
 		ptr = ptr->next;
 	}
