15a2a91e4b67debed6330660fca2de2fd6d6a96c
Younesid12 <idyounes95@gmail.com>
2024-01-13 05:54:27 -0800

    adds a new node at the end if a dlistint_t list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -26,6 +26,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		new_node->prev = NULL;
 		new_node->n = n;
 		new_node->next = NULL;
+		*head = new_node;
 		return (new_node);
 	}
 	ptr = *head;
