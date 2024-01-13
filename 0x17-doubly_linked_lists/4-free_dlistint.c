817533aa17bb88f8734d8b50c556539da926ee19
Younesid12 <idyounes95@gmail.com>
2024-01-13 07:09:21 -0800

    frees a dlistint_t list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: pointer to the head node of the list
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *ptr;
+
+	ptr = head;
+	while(ptr != NULL)
+	{
+		head = head->next;
+		free(ptr);
+		ptr = head;
+	}
+	head = NULL;
+}
