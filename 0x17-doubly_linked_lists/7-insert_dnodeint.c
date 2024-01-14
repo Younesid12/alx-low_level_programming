593195096a4b5e115d54a2b7583dd4f4eebcd83e
Younesid12 <idyounes95@gmail.com>
2024-01-14 09:01:49 -0800

    inserts a new node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -45,10 +45,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new_node->prev = temp;
 			new_node->next = ptr;
 			ptr->prev = new_node;
-			if (ptr->next != NULL)
-			{
-				ptr->next->prev = new_node;
-			}
 			return (new_node);
 		}
 		i++;
