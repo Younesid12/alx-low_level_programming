fb8a652138404396c73988bd2e0e1b42f5c06fdf
Younesid12 <idyounes95@gmail.com>
2024-04-06 14:26:54 -0700

    creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,11 +13,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	new_hash->size = size;
-	new_hash->array = (hash_node_t **)calloc(size, sizeof(hash_node_t));
+	new_hash->array = (hash_node_t **)calloc(size, sizeof(hash_node_t *));
 	if (!new_hash->array)
 	{
 		return (NULL);
 	}
 	return (new_hash);
 }
-
