b18a29d3e19eb9ae98e03f6f25a49118d8d70552
Younesid12 <idyounes95@gmail.com>
2024-04-06 14:09:31 -0700

    creates a hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -9,7 +9,7 @@
  * The key is unique in the HashTable
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the List
- */
+*/
 typedef struct hash_node_s
 {
      char *key;
@@ -24,7 +24,7 @@ typedef struct hash_node_s
  * @array: An array of size @size
  * Each cell of this array is a pointer to the first node of a linked list,
  * because we want our HashTable to use a Chaining collision handling
- */
+*/
 typedef struct hash_table_s
 {
      unsigned long int size;
