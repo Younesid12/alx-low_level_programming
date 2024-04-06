216a438a82d441a256c37011b0163e5e33cf8f80
Younesid12 <idyounes95@gmail.com>
2024-04-06 13:53:26 -0700

    creates a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size: the saize of the array
+ * Return: a pointer to the newly created hash table, otherwise NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash = malloc(sizeof(hash_table_t));
+	if (!new_hash)
+	{
+		return (NULL);
+	}
+	new_hash->size = size;
+	new_hash->array = (hash_table_s **)calloc(new_hash->size, sizeof(hash_node_t));
+	if (!new_hash->array)
+	{
+		return (NULL);
+	}
+	return (new_hash);
+e}
+
