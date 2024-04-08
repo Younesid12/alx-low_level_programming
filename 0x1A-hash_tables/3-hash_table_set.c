e7a7501e359204875a4544c6313657a10942253a
Younesid12 <idyounes95@gmail.com>
2024-04-07 19:53:07 -0700

    updated

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -41,9 +41,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	strcpy(new_node->key, key);
 	strcpy(new_node->value, value);
 	new_node->next = NULL;
-	if (!ht->array[index])
+	if (ht->array[index])
 	{
 		new_node->next = ht->array[index];
+		ht->array[index] = new_node
 		return (1);
 	}
 	new_node->next = ht->array[index];
