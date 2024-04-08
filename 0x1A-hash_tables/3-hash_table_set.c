6aaa8bd8c316afcb5668aa46022f18585381f780
Younesid12 <idyounes95@gmail.com>
2024-04-07 19:53:37 -0700

    updated

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -44,7 +44,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht->array[index])
 	{
 		new_node->next = ht->array[index];
-		ht->array[index] = new_node
+		ht->array[index] = new_node;
 		return (1);
 	}
 	new_node->next = ht->array[index];
