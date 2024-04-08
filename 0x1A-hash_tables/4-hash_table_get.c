d464be77038b0c748c5f5d7a7a70eb3fa77e6a37
Younesid12 <idyounes95@gmail.com>
2024-04-08 15:01:08 -0700

    retrieves a value associated with a key.

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,11 +9,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 	hash_node_t *temp;
-	if (!ht)
+	
+	if (!ht || !key)
 	{
 		return (NULL);
 	}
-
 	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index]->key == NULL)
 	{
