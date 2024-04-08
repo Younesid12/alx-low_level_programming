7f3c36392445204143d33e5eb7c8fd12fd657588
Younesid12 <idyounes95@gmail.com>
2024-04-07 19:45:10 -0700

    updated

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -48,6 +48,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		new_node->next = ht->array[index];
 		return (1);
 	}
+	new_node->next = ht->array[index]
 	ht->array[index] = new_node;
 	return (1);
 }
