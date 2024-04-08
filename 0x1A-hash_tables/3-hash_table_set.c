7bf8372990bfded41b6bffecdd6551a90415a8b0
Younesid12 <idyounes95@gmail.com>
2024-04-07 19:50:18 -0700

    updated

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *n;
 
 	if (!key || strlen(key) == 0 || ht == NULL)
-		return (0);}
+		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index] && strcmp(ht->array[index]->key, key) == 0)
 	{
