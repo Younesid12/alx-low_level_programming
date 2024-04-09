8c7a46822c985a2a817539ecce652c66be150824
Younesid12 <idyounes95@gmail.com>
2024-04-09 16:19:45 -0700

    prints a hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -19,7 +19,7 @@ void hash_table_print(const hash_table_t *ht)
 		temp = ht->array[i];
 		while (temp)
 		{
-			if (temp == ht->array[0])
+			if (temp != ht->array[0])
 			{
 				printf(", ");
 			}
