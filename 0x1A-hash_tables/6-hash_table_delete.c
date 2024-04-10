0f6439b7005717d30d0415838569859f97df32f9
Younesid12 <idyounes95@gmail.com>
2024-04-09 18:13:16 -0700

    deletes a hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,27 @@
+#include "hasht_tables.h"
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *ptr;
+	unsigned int i;
+	hash_node_t = temp;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		ptr = ht->array[i]
+		while(ptr)
+		{
+			free(ptr->key);
+			free(ptr->value);
+			temp = ptr;
+			ptr = ptr->next;
+			free(temp);
+		}
+	}
+	free(ht);
+}
+
